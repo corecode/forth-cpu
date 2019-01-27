740a41721efb0f5b5d0a7191f15d81f86346ed4c
Simon Schubert <2@0x2c.org>
2019-01-27 21:07:11 +0100

    start unit testing opcodes

--- tb/forth_tb.v
+++ tb/forth_tb.v
@@ -1,3 +1,10 @@
+`define assert_eq(v1, v2) \
+if(v1 != v2) begin \
+   $display("%s:%d: %s == %s failed: %x != %x", `__FILE__,`__LINE__,`"v1`",`"v2`",v1,v2); \
+   $finish(1); \
+     end
+
+
 module forth_tb;
 
    logic clk;
@@ -20,18 +27,12 @@ initial begin
 end
 
 initial begin
-   reset = 1;
-   #95 reset = 0;
-end
-
-initial begin
    clk             = 0;
    forever #10 clk = !clk;
 end
 
+task run_program;
    logic [15:0] imem[0:127];
-
-task init_mem;
    int i;
 
    for (int i = $low(imem); i < $high(imem); i++)
@@ -41,14 +42,44 @@ task init_mem;
    imem[i++]   = 1;
    imem[i++]   = 2;
    imem[i++]   = 'he007;
+
+   forever
+     @(posedge clk) idata = imem[iaddr];
 endtask
 
-initial
-  init_mem();
 
-initial
-  forever
-    @(posedge clk) idata = imem[iaddr];
+   typedef logic [15:0] val;
 
+task test_opcode(string mnem, logic [15:0] op, val IP_e, val PSP_e, val RSP_e, val TOS_e);
+   $display("testing opcode %h, %s", op, mnem);
+
+   reset = 1;
+   @(posedge clk) #5;
+   uut.IP = 100;
+   reset = 0;
+   idata = op;
+   @(posedge clk);              // fetch
+   @(posedge clk);              // execute
+
+   #1;                          // let new signals manifest
+   `assert_eq(uut.IP, IP_e);
+   `assert_eq(uut.PSP, PSP_e);
+   `assert_eq(uut.RSP, RSP_e);
+   `assert_eq(uut.TOS, TOS_e);
+endtask
+
+typedef enum {
+              OP_NOP = 'he040,
+              OP_AND = 'he007
+      } opcodes;
+
+initial begin
+   // mnem, op, IP, PSP, RSP, TOS
+   test_opcode("lit 0", 'h0000, 101, 1, 0, 0);
+   test_opcode("lit 7fff", 'h7fff, 101, 1, 0, 'h7fff);
+   test_opcode("NOP", OP_NOP, 101, 0, 0, 0);
+   //test_opcode("AND", OP_AND, 101, 0, 0, 0);
+   $finish;
+end
 
 endmodule
