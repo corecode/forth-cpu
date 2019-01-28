dfbca9015c653775fd26981db3b1551a2932a4d5
Simon Schubert <2@0x2c.org>
2019-01-28 01:59:22 +0100

    convert to verilog that makes synplify happy

--- rtl/forth.v
+++ rtl/forth.v
@@ -1,7 +1,3 @@
-`ifndef unique
- `define unique unique
-`endif
-
 module forth
   (
    clk,
@@ -24,35 +20,33 @@ parameter daddr_width = 8;
 localparam instr_width = 16;
 localparam stack_width = $clog2(stacksize);
 
-   input logic clk;
-   input logic reset;
+   input clk;
+   input reset;
 
-   output wire [iaddr_width-1:0] iaddr;
-   input logic [instr_width-1:0] idata;
+   output [iaddr_width-1:0] iaddr;
+   input [instr_width-1:0]  idata;
 
-   output logic [daddr_width-1:0] daddr;
-   input logic [width-1:0]        ddata_read;
-   output logic [width-1:0]       ddata_write;
-   output logic                   dwrite;
+   output [daddr_width-1:0] daddr;
+   input [width-1:0]        ddata_read;
+   output [width-1:0]       ddata_write;
+   output                   dwrite;
 
-   logic [instr_width-1:0]        instr;
+   wire [instr_width-1:0]   instr;
 
-   logic [stack_width-1:0]        PSP;
-   wire [stack_width-1:0]         PSP_next;
-   logic [stack_width-1:0]        RSP;
-   wire [stack_width-1:0]         RSP_next;
-   logic [iaddr_width-1:0]        IP;
-   logic [iaddr_width-1:0]        IP_next;
-   logic [width-1:0]              TOS;
-   logic [width-1:0]              TOS_next;
+   reg [stack_width-1:0]    PSP;
+   wire [stack_width-1:0]   PSP_next;
+   reg [stack_width-1:0]    RSP;
+   wire [stack_width-1:0]   RSP_next;
+   reg [iaddr_width-1:0]    IP;
+   reg [iaddr_width-1:0]    IP_next;
+   reg [width-1:0]          TOS;
+   reg [width-1:0]          TOS_next;
 
-   logic [width-1:0]              pstack[0:stacksize-1];
-   logic [stack_width-1:0]        pstack_addr;
-   logic [width-1:0]              pstack_top;
+   reg [width-1:0]          pstack[0:stacksize-1];
+   reg [width-1:0]          pstack_top;
 
-   logic [width-1:0]              rstack[0:stacksize-1];
-   logic [stack_width-1:0]        rstack_addr;
-   logic [width-1:0]              rstack_top;
+   reg [width-1:0]          rstack[0:stacksize-1];
+   reg [width-1:0]          rstack_top;
 
 
 /*
@@ -88,52 +82,45 @@ localparam stack_width = $clog2(stacksize);
 
  */
 
-   enum logic [2:0]
-        {
-         O_NOT  = 3'b000,
-         O_ASHR = 3'b001,
-         O_EQ0  = 3'b010,
-         O_NEG  = 3'b011,
-         O_AND  = 3'b100,
-         O_OR   = 3'b101,
-         O_XOR  = 3'b110,
-         O_ADD  = 3'b111
-         }
-        o_alu;
+`define O_NOT  3'b000
+`define O_ASHR 3'b001
+`define O_EQ0  3'b010
+`define O_NEG  3'b011
+`define O_AND  3'b100
+`define O_OR   3'b101
+`define O_XOR  3'b110
+`define O_ADD  3'b111
+   wire [2:0]               o_alu;
+   wire                     o_psp_en;
+   wire                     o_psp_dir;
+   wire                     o_rsp_en;
+   wire                     o_rsp_dir;
 
-   wire                           o_psp_en;
-   wire                           o_psp_dir;
-   wire                           o_rsp_en;
-   wire                           o_rsp_dir;
-   enum logic [1:0]
-        {
-         O_ALU    = 'b00,
-         O_TOS    = 'b01,
-         O_PSTACK = 'b10,
-         O_RSTACK = 'b11
-         }
-        o_tos_sel;
+`define O_ALU    2'b00
+`define O_TOS    2'b01
+`define O_PSTACK 2'b10
+`define O_RSTACK 2'b11
+   wire [1:0]               o_tos_sel;
 
-   enum logic [1:0]
-        {
-         O_IP_IMM     = 'b00,
-         O_IP_CONDIMM = 'b01,
-         O_IP_TOS     = 'b10,
-         O_IP_INC     = 'b11
-         }
-        o_ipsel;
+`define O_IP_IMM     2'b00
+`define O_IP_CONDIMM 2'b01
+`define O_IP_TOS     2'b10
+`define O_IP_INC     2'b11
+   wire [1:0]               o_ipsel;
 
-   logic o_ret;
+   wire                     o_ret;
 
-   logic [width-2:0] o_imm;
-   logic [iaddr_width-1:0] o_imm_pc;
+   wire                     o_is_imm;
+   wire [width-2:0]         o_imm;
+   wire [iaddr_width-1:0]   o_imm_pc;
 
-assign o_imm     = !instr[instr_width-1];
+assign o_is_imm  = ~instr[instr_width-1];
+assign o_imm     = instr[width-2:0];
 
 assign o_alu     = instr[2:0];
-assign o_psp_en  = instr[2] | o_imm;
-assign o_psp_dir = instr[3] | o_imm;
-assign o_rsp_en  = (instr[4] | o_ret) & !o_imm;
+assign o_psp_en  = instr[2] | o_is_imm;
+assign o_psp_dir = instr[3] | o_is_imm;
+assign o_rsp_en  = (instr[4] | o_ret) & !o_is_imm;
 assign o_rsp_dir = instr[5] & !o_ret;
 assign o_tos_sel = instr[7:6];
 assign o_ret     = instr[instr_width-4];
@@ -142,7 +129,7 @@ assign o_ipsel   = instr[instr_width-2:instr_width-3];
 assign o_imm_pc  = instr[iaddr_width-1:0];
 
 
-   logic [instr_width-1:0] OP_NOP = 'he040;
+`define OP_NOP 16'he040
 
 // | 15  | 14  | 13  | 12  | ...
 // | imm |   ipsel   | ret | ...
@@ -151,10 +138,10 @@ assign o_imm_pc  = instr[iaddr_width-1:0];
 
 // conditional signals ///////////////////////////
 
-   logic TOS_is_zero;
+   wire                  TOS_is_zero;
 assign TOS_is_zero = !(|TOS);
 
-   logic need_wait;
+   reg need_wait;
 always @(posedge clk)
   if (reset)
     need_wait <= 1;
@@ -163,23 +150,22 @@ always @(posedge clk)
 
 // instruction fetch /////////////////////////////
 
-   logic [iaddr_width-1:0] IP_inc;
+   wire [iaddr_width-1:0] IP_inc;
 assign IP_inc = IP + 1;
 
 always @(*)
-  `unique
-    case (1'b1)
-      need_wait: IP_next = IP;
-      o_imm: IP_next     = IP_inc;
-      o_ret: IP_next     = rstack_top;
-      default:
-        case (o_ipsel)
-          O_IP_IMM    : IP_next = o_imm_pc;
-          O_IP_CONDIMM: IP_next = TOS_is_zero ? o_imm_pc : IP_inc;
-          O_IP_INC    : IP_next = IP_inc;
-          O_IP_TOS    : IP_next = TOS;
-        endcase
-    endcase
+  case (1'b1)
+    need_wait: IP_next = IP;
+    o_is_imm: IP_next     = IP_inc;
+    o_ret: IP_next     = rstack_top;
+    default:
+      case (o_ipsel)
+        `O_IP_IMM    : IP_next = o_imm_pc;
+        `O_IP_CONDIMM: IP_next = TOS_is_zero ? o_imm_pc : IP_inc;
+        `O_IP_INC    : IP_next = IP_inc;
+        `O_IP_TOS    : IP_next = TOS;
+      endcase
+  endcase
 
 always @(posedge clk)
   if (reset)
@@ -188,19 +174,17 @@ always @(posedge clk)
     IP <= IP_next;
 
 assign iaddr = IP_next;
-assign instr = need_wait ? OP_NOP : idata;
+assign instr = need_wait ? `OP_NOP : idata;
 
 // RSP ///////////////////////////////////////////
 
-   logic [stack_width-1:0] RSP_inc;
-
+   reg [stack_width-1:0]  RSP_inc;
 always @(*)
-  `unique
-    casex ({o_rsp_en, o_rsp_dir})
-      2'b0?: RSP_inc = 0;
-      2'b10: RSP_inc = -1;
-      2'b11: RSP_inc = 1;
-    endcase
+  casex ({o_rsp_en, o_rsp_dir})
+    2'b0?: RSP_inc = 0;
+    2'b10: RSP_inc = -1;
+    2'b11: RSP_inc = 1;
+  endcase
 
 assign RSP_next = RSP + RSP_inc;
 
@@ -210,8 +194,8 @@ always @(posedge clk)
   else
     RSP <= RSP_next;
 
-   logic [iaddr_width-1:0] rstack_next;
-assign rstack_next = o_ipsel == O_IP_INC ? TOS : IP_next;
+   wire [width-1:0]       rstack_next;
+assign rstack_next = o_ipsel == `O_IP_INC ? TOS : IP_next;
 
 always @(posedge clk)
   if (o_rsp_en && o_rsp_dir)
@@ -225,15 +209,13 @@ always @(posedge clk)
 
 // PSP ///////////////////////////////////////////
 
-   logic [stack_width-1:0]        PSP_inc;
-
+   reg [stack_width-1:0]  PSP_inc;
 always @(*)
-  `unique
-    casex ({o_psp_en, o_psp_dir})
-      2'b0?: PSP_inc = 0;
-      2'b10: PSP_inc = -1;
-      2'b11: PSP_inc = 1;
-    endcase
+  casex ({o_psp_en, o_psp_dir})
+    2'b0?: PSP_inc = 0;
+    2'b10: PSP_inc = -1;
+    2'b11: PSP_inc = 1;
+  endcase
 
 assign PSP_next = PSP + PSP_inc;
 
@@ -256,38 +238,35 @@ always @(posedge clk)
 
 // ALU ///////////////////////////////////////////
 
-   wire [width-1:0]               ain1, ain2;
-   logic [width-1:0]              alu_out;
+   wire [width-1:0]       ain1, ain2;
+   reg [width-1:0]        alu_out;
 assign ain1 = TOS;
 assign ain2 = pstack_top;
 
 always @(*)
-  `unique
-    case (o_alu)
-      O_ASHR: alu_out = {ain1[width-1],ain1[width-1:1]};
-      O_NOT: alu_out  = ~ain1;
-      O_EQ0: alu_out  = TOS_is_zero ? -1 : 0;
-      O_NEG: alu_out  = -ain1;
-      O_AND: alu_out  = ain1 & ain2;
-      O_OR: alu_out   = ain1 | ain2;
-      O_XOR: alu_out  = ain1 ^ ain2;
-      O_ADD: alu_out  = ain1 + ain2;
-      //    O_SUB: alu_out  = ain - ain2;
-    endcase
+  case (o_alu)
+    `O_ASHR: alu_out = {ain1[width-1],ain1[width-1:1]};
+    `O_NOT: alu_out  = ~ain1;
+    `O_EQ0: alu_out  = TOS_is_zero ? -1 : 0;
+    `O_NEG: alu_out  = -ain1;
+    `O_AND: alu_out  = ain1 & ain2;
+    `O_OR: alu_out   = ain1 | ain2;
+    `O_XOR: alu_out  = ain1 ^ ain2;
+    `O_ADD: alu_out  = ain1 + ain2;
+  endcase
 
 // TOS ///////////////////////////////////////////
 
 always @(*)
-  if (o_imm)
-    TOS_next = {1'b0, instr[instr_width-2:0]};
+  if (o_is_imm)
+    TOS_next = {1'b0, o_imm};
   else
-    `unique
-      case (o_tos_sel)
-        O_TOS:    TOS_next = TOS;
-        O_ALU:    TOS_next = alu_out;
-        O_PSTACK: TOS_next = pstack_top;
-        O_RSTACK: TOS_next = rstack_top;
-      endcase
+    case (o_tos_sel)
+      `O_TOS:    TOS_next = TOS;
+      `O_ALU:    TOS_next = alu_out;
+      `O_PSTACK: TOS_next = pstack_top;
+      `O_RSTACK: TOS_next = rstack_top;
+    endcase
 
 always @(posedge clk)
   if (reset)
